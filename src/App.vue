a015aaf57fd44a8cfccdda2cf3c724dc06bc19ff
Wind <zhangyf@phxg.cn>
2023-04-26 17:43:12 +0800

    chore: remove page plugin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
+<script setup>
+const text = ref('LazyTooltipLazyTooltipLazyTooltip')
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
+    <LazyTooltip max-w-20 :text="text">
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        :content="text"
+        placement="top-start"
+      >
+        <span inline-block w-full truncate>{{ text }}</span>
+      </el-tooltip>
+    </LazyTooltip>
     <TheFooter />
   </main>
 </template>
